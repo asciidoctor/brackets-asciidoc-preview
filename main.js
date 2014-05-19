023e124f7312fbe1ee46587f201afdc51efe8a8b
Thomas Kern <kern99@web.de>
2014-05-19 17:33:33 +0200

    add missing spaces

--- main.js
+++ main.js
@@ -115,9 +115,9 @@ define(function (require, exports, module) {
             }
 
             var defaultAttributes = 'icons=font@' 
-                                  + 'platform=opal platform-opal'
-                                  + 'env=browser env-browser'
-                                  + 'source-highlighter=highlight.js';
+                                  + ' platform=opal platform-opal'
+                                  + ' env=browser env-browser'
+                                  + ' source-highlighter=highlight.js';
             var numbered = _prefs.get("numbered") ? 'numbered' : 'numbered!';
             var showtitle = _prefs.get("showtitle") ? 'showtitle' : 'showtitle!';
             var safemode = _prefs.get("safemode") || "safe";
