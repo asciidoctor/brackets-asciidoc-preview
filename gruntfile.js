e46c0772b5292c5df371d3866f93d2bdb3320107
nerk <kern99@web.de>
2015-01-18 16:25:48 +0100

    'devdeploy' task for development under Windows added

--- gruntfile.js
+++ gruntfile.js
@@ -1,72 +1,122 @@
 var path = require('path');
 
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+module.exports = function (grunt) {
 
-    // clean dist task
-    clean: {
-      options: {
-        dot: true
-      },
-      dist: ['dist/*']
-    },
+    var distfiles = ['images/**', 'lib/**', 'mode/**', 'styles/**', 'templates/**', 'themes/**',
+        'CHANGELOG.adoc', 'LICENSE.txt', 'README.adoc', 'main.js', 'package.json', '!**/Thumbs.db'];
 
-    /**
-    * Copy files to dist
-    */
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          src: ['images/**', 'lib/**', 'mode/**', 'styles/**', 'templates/**', 'themes/**',
-              'CHANGELOG.adoc', 'LICENSE.txt', 'README.adoc', 'main.js', 'package.json', '!**/Thumbs.db'],
-          dest: 'dist/'
-        }]
-      }
-    },
+    var pkg = grunt.file.readJSON('package.json');
 
-    /**
-    * Create distribution zip file
-    */
-    compress: {
-      main: {
-        options: {
-          archive: '<%= pkg.name %>-<%= pkg.version %>.zip'
+    // For development and Windows only:
+    // extensionDir is the directory where this extension is installed
+    // Just run 'grunt devdeploy' to copy the distribution files
+    // to the brackets extension directory.
+    //
+    // In Unix environments this is not necessary. We can just use a symbolic link from
+    // the extension directory to the source directory.
+
+    var devExtFiles = [];
+    if (process.platform === 'win32') {
+        var extensionDir = process.env.APPDATA + '/Brackets/extensions/user/' + pkg.name + '/';
+        devExtFiles = [extensionDir + '*'];
+    }
+
+    grunt.initConfig({
+        pkg: pkg,
+
+        clean: {
+            /*
+             * clean distribution directory
+             */
+            dist: {
+                options: {
+                    dot: true
+                },
+                src: ['dist/*']
+            },
+            /*
+             * clean external extension directory (Windows only)
+             */
+            dev: {
+                options: {
+                    dot: true,
+                    force: true
+                },
+                src: devExtFiles
+            }
         },
-        files: [
-          {expand: true, cwd: 'dist/', src: ['**']}
-        ]
-      }
-    },
 
-    /**
-     * Install asciidoctor.css to ./themes and 
-     * asciidoctor-all.min.js to ./lib
-     * (needs "exportsOverride" in bower.json)
-     */
-    bower: {
-        install: {
-            options: {
-                verbose: false,
-                cleanBowerDir: true,
-                targetDir: './lib',
-                layout: function(type, component, src) {
-                    if (type === "css") {
-                        return path.join("../themes");
+        copy: {
+            /**
+             * Copy files to dist
+             */
+            dist: {
+                files: [{
+                    expand: true,
+                    src: distfiles,
+                    dest: 'dist/'
+                }]
+            },
+            /**
+             * Copy files to brackets extension directory (Windows only)
+             */
+            dev: {
+                files: [{
+                    expand: true,
+                    src: distfiles,
+                    dest: extensionDir
+                }]
+            }
+        },
+
+        /**
+         * Create distribution zip file
+         */
+        compress: {
+            main: {
+                options: {
+                    archive: '<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                files: [
+                    {expand: true, cwd: 'dist/', src: ['**']}
+                ]
+            }
+        },
+
+        /**
+         * Install asciidoctor.css to ./themes and
+         * asciidoctor-all.min.js to ./lib
+         * (needs "exportsOverride" in bower.json)
+         */
+        bower: {
+            install: {
+                options: {
+                    verbose: false,
+                    cleanBowerDir: true,
+                    targetDir: './lib',
+                    layout: function (type, component, src) {
+                        if (type === "css") {
+                            return path.join("../themes");
+                        }
+                        return "";
                     }
-                    return "";
                 }
             }
-        }  
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-bower-task');
+        }
+    });
 
-  grunt.registerTask('default', ['dist']);
-  grunt.registerTask('dist', ['clean', 'bower', 'copy', 'compress']);
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.registerTask('default', ['dist']);
+    grunt.registerTask('dist', ['clean:dist', 'bower', 'copy:dist', 'compress']);
+
+    // Development under Windows only
+    if (extensionDir) {
+        grunt.registerTask('devdeploy', ['clean:dev', 'copy:dev']);
+    }
 };
 
